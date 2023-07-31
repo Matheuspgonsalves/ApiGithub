a99afc61123829a9d6ea7ad1ceb7dfb4cd971a4f
Matheus Pereira Gonsalves <matheus.gonsalvespereira@gmail.com>
2023-07-31 15:29:58 -0300

    Primeiro commit, site completo

---
+++ script.js
@@ -0,0 +1,57 @@
+const apiUrl = "https://api.github.com/users/"
+const xhr = new XMLHttpRequest();
+var obj;
+var divConteudo = document.querySelector("#conteudo");
+var username = document.querySelector("#name");
+var form = document.querySelector("#form");
+var btnEnviar = document.querySelector("#enviar")
+
+form.addEventListener("submit", execRequest);
+
+function execRequest(e){
+    
+    e.preventDefault();
+    xhr.onload = function (){
+        if(xhr.readyState == 4 && xhr.status == 200) {
+            obj = JSON.parse(xhr.response);
+            divConteudo.innerHTML = 
+            `
+            <div id="infosGit">
+                <span><img src="${obj.avatar_url}"> </span>
+                <div id="camposBio">
+                    <span>Nome completo: ${obj.name}</span>
+                    <span>Nome de Usuário: ${obj.login}</span>
+                    <span>Biografia: ${obj.bio}</span>
+                    <span>Link do perfil: <a href="${obj.html_url}" target="_blank">${obj.html_url}</a></span>
+                </div>
+                <div id="refresh">
+                    <input type="button" value="Voltar" id="voltar">
+                </div>
+            </div>
+            `
+            document.querySelector("#voltar").addEventListener('click', () => {
+                location.reload()
+            });
+        }else {
+            obj = JSON.parse(xhr.response);
+            divConteudo.innerHTML = 
+            `
+            <div id="errMsg">
+                Usuário não encontrado...
+            </div>
+            `
+        }
+        
+    }
+    xhr.open("GET", apiUrl + username.value, true);
+    xhr.send();
+}
+
+
+
+
+
+
+
+
+
